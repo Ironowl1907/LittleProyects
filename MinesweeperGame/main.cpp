8417da6ea0d93cdc25c5375344c59940625c241f
Facundo <facuguialtana7@gmail.com>
2023-12-23 00:29:06 -0300

    MineSweeper T1

---
+++ MinesweeperGame/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+
+
+int main(){
+  // Create an empty map of 30 x 30
+  std::vector<std::vector<char>> map(30,std::vector<char>(30,'.'));
+
+  // Create a list of the bomb coords
+  std::vector<std::vector<int>> BombList;
+  return 0;
+}
