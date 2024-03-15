c7015994417996555d13e95d5c920cc9695e9fa9
IronOwl <facuguiantana7@gmail.com>
2024-03-15 17:37:49 -0300

    Ordering files and adding a Makefile

--- ReagularExpressionCalculator/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#include <cctype>
-using namespace std;
-
-int main (int argc, char *argv[]) {
-  cout << isalpha('7') << endl;
-  cout << isdigit('r') << endl;
-  return 0;
-}
