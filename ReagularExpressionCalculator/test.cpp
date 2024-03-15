5a07bf4007a322da75e67f2c9860cdca46a8298c
IronOwl <facuguiantana7@gmail.com>
2024-03-15 17:32:02 -0300

    Regular Expression Calculator first commit (adding a basic lexer)

---
+++ ReagularExpressionCalculator/test.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include <cctype>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+  cout << isalpha('7') << endl;
+  cout << isdigit('r') << endl;
+  return 0;
+}
